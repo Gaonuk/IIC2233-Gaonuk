ddad1f663a46d8348ee5a70953240755f53f1245
Gaonuk <rodrigogaonagonzalez@gmail.com>
2019-08-17 14:35:54 -0400

    Added the method that calculates the surrounding legos + gameOver method for ending the game

--- Tareas/T00/game.py
+++ Tareas/T00/game.py
@@ -5,8 +5,15 @@ import os
 import random 
 import math
 import sys
+
+#Variables globales
 baldosasVacias = 0
+baldDesc = 0
+puntaje = 0
 letras = 'ABCDEFGHIJKLMNO'
+usuario = ''
+numLegos = 0
+
 
 def crearTablero(n, m):
     largo = [[' ' for x in range(m)] for x in range(n)]
@@ -27,7 +34,9 @@ def printMenu():
 
     
 def menuPrincipal(resp):
+    global numLegos
     global baldosasVacias
+    global usuario
     if resp == '0':
         return 
     elif resp == '1':
@@ -53,7 +62,11 @@ def menuPrincipal(resp):
             menuPrincipal('1')
         usuario = input('Por favor, introduzca su nombre de usuario:  ')
         
-        tabla = crearTablero(int(n) , int(m))
+        # Se crean dos tableros distintos, uno contiene la posicion de los Legos 
+        # y el otro es el tablero que se le muestra al usuario
+
+        tablaUsuario = crearTablero(int(n) , int(m))
+        tabla = crearTablero(int(n), int(m))
         
         # tablero.print_tablero(tabla)
         
@@ -74,7 +87,10 @@ def menuPrincipal(resp):
                 if k != 'L':
                     baldosasVacias += 1
 
-        printJuego(tabla)
+
+        tablero.print_tablero(tabla)
+        printJuego(tablaUsuario, tabla)
+
 
     elif resp == '2':
         return 2
@@ -85,40 +101,40 @@ def menuPrincipal(resp):
         menuPrincipal(resp)
 
 
-def printJuego(tabla):
-    tablero.print_tablero(tabla)
-    print(tabla)
+def printJuego(tablaUsuario, mainTabla):
+    tablero.print_tablero(tablaUsuario)
     print('Seleccione una de las siguientes acciones:')
     print(' [1] Descubrir una baldosa')
     print(' [2] Guardar la partida')
     print(' [0] Salir de la partida')
     opcion = input('Indique su opcion (0, 1 o 2):  ')
     if opcion.isdigit():
-        mainJuego(tabla, opcion)
+        mainJuego(tablaUsuario, opcion, mainTabla)
     else:
         print('Respuesta Invalida!')
-        printJuego(tabla)
+        printJuego(tablaUsuario, mainTabla)
 
 
-def mainJuego(tabla, opcion):
+def mainJuego(tablaUsuario, opcion, mainTabla):
     global baldosasVacias
     global letras
+    global baldDesc
     if opcion == '1':
         print('Por favor indique las coordenadas de la baldosa que desea descubrir:')
         c1 = input('Coordenada 1 (numeros): ')
         c2 = input('Coordenada 2 (letras): ')
         if not c1.isdigit() or not c2.isalpha():
             print('Coordenadas invalidas.')
-            mainJuego(tabla, '1')
-        elif int(c1) >= len(tabla):
+            mainJuego(tablaUsuario, '1', mainTabla)
+        elif int(c1) >= len(tablaUsuario):
             print('Coordenada fuera del rango valido')
-            mainJuego(tabla, '1')
-        elif ord(c2) < ord('A') or ord(c2) > ord(letras[len(tabla)-1]):
+            mainJuego(tablaUsuario, '1', mainTabla)
+        elif ord(c2) < ord('A') or ord(c2) > ord(letras[len(tablaUsuario)-1]):
             print('Coordenada fuera del rango valido')
-            mainJuego(tabla, '1')
+            mainJuego(tablaUsuario, '1', mainTabla)
 
         else:
-            print(baldosasVacias)
+           
             c1 = int(c1)
             for l, i in enumerate(letras):
                 if i == c2:
@@ -126,24 +142,99 @@ def mainJuego(tabla, opcion):
                     break
             minasAledanas = 0
 
-            # if c1 == 0:
-            #     if c3 == 0:
-            if tabla[c1][c3] == 'L':
-                minasAledanas += 1
-            if tabla[c1][c3 + 1] == 'L':
-                minasAledanas += 1
-            if tabla[c1 + 1][c3] == 'L':
-                minasAledanas += 1
-            if tabla[c1 + 1][c3 + 1] == 'L':
-                minasAledanas += 1
-            tabla[c1][c3] = minasAledanas
+            if mainTabla[c1][c3] == 'L':
+                gameOver(mainTabla)
 
-            printJuego(tabla)
+            elif c1 == 0:
+                if c3 == 0:
+                    if mainTabla[c1][c3 + 1] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 + 1][c3] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 + 1][c3 + 1] == 'L':
+                        minasAledanas += 1
+                    tablaUsuario[c1][c3] = minasAledanas
+                    baldDesc += 1
+                    printJuego(tablaUsuario, mainTabla)
+                elif c3 == len(mainTabla[0])-1 :
+                    if mainTabla[c1][c3 - 1] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 + 1][c3] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 + 1][c3 - 1] == 'L':
+                        minasAledanas += 1
+                    tablaUsuario[c1][c3] = minasAledanas
+                    baldDesc += 1
+                    printJuego(tablaUsuario, mainTabla)
+                else:
+                    if mainTabla[c1][c3 + 1] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1][c3 - 1] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 + 1][c3] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 + 1][c3 + 1] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 + 1][c3 - 1] == 'L':
+                        minasAledanas += 1
+                    tablaUsuario[c1][c3] = minasAledanas
+                    baldDesc += 1
+                    printJuego(tablaUsuario, mainTabla)
+            elif c1 == len(mainTabla)-1:
+                if c3 == 0:
+                    if mainTabla[c1][c3 + 1] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 - 1][c3] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 - 1][c3 + 1] == 'L':
+                        minasAledanas += 1
+                    tablaUsuario[c1][c3] = minasAledanas
+                    baldDesc += 1
+                    printJuego(tablaUsuario, mainTabla)
+                elif c3 == len(mainTabla[0])-1 :
+                    if mainTabla[c1][c3 - 1] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 - 1][c3] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 - 1][c3 - 1] == 'L':
+                        minasAledanas += 1
+                    tablaUsuario[c1][c3] = minasAledanas
+                    baldDesc += 1
+                    printJuego(tablaUsuario, mainTabla)
+                else:
+                    if mainTabla[c1][c3 + 1] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1][c3 - 1] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 - 1][c3] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 - 1][c3 + 1] == 'L':
+                        minasAledanas += 1
+                    if mainTabla[c1 - 1][c3 - 1] == 'L':
+                        minasAledanas += 1
+                    tablaUsuario[c1][c3] = minasAledanas
+                    baldDesc += 1
+                    printJuego(tablaUsuario, mainTabla)
+
+
+            
                 
     elif opcion == '2':
         pass
     else:
         pass
+
+
+def gameOver(tabla):                   
+    global usuario 
+    global puntaje 
+    global baldDesc
+    global numLegos
+    print('Te has topado con un Lego! Perdiste la partida ' + usuario + '!')
+    tablero.print_tablero(tabla)
+    puntaje = numLegos * baldDesc * parametros.POND_PUNT
+    print('Puntaje final : ' + str(puntaje))
+    rank = open('puntajes.txt', 'a')
 
 
 if __name__ == "__main__":
