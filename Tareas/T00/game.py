4c4ea3347db40fa57b81e0a814a53024b35c68b9
Gaonuk <rodrigogaonagonzalez@gmail.com>
2019-08-17 19:01:30 -0400

    Added the victory mechanism

--- Tareas/T00/game.py
+++ Tareas/T00/game.py
@@ -15,25 +15,29 @@ usuario = ''
 numLegos = 0
 
 
+# Funcion que crea el tablero del juego en funcion de los 
+# datos introducidos por el usuario 
 def crearTablero(n, m):
     largo = [[' ' for x in range(m)] for x in range(n)]
     return largo
 
 
+# Funcion que imprime el menu principal del juego 
 def printMenu():
     print('*******************************************************')
     print('               BIENVENIDO A LEGOSWEEPER!               ')
-    print('*******************************************************')
-    print(' Seleccione una opcion:  ')
-    print('     [1] Crear partida   ')
-    print('     [2] Cargar partida  ')
-    print('     [3] Ver ranking     ')
-    print('     [0] Salir           ')
+    print('*******************************************************\n')
+    print(' Seleccione una opcion:  \n')
+    print('     [1] Crear partida   \n')
+    print('     [2] Cargar partida  \n')
+    print('     [3] Ver ranking     \n')
+    print('     [0] Salir           \n')
     resp = input('Indique su resp (0, 1, 2 o 3):  ')
     return resp 
 
-    
-def menuPrincipal(resp):
+
+
+def menuPrincipal(resp): 
     global numLegos
     global baldosasVacias
     global usuario
@@ -45,20 +49,20 @@ def menuPrincipal(resp):
             if int(n) <= 15 and int(n) >= 3:
                 pass
             else:
-                print('Valor fuera del rango establecido')
+                print('Valor fuera del rango establecido\n')
                 menuPrincipal('1')
         else:
-            print('Respuesta Invalida.')
+            print('Respuesta Invalida.\n')
             menuPrincipal('1')
         m = input('Por favor, introduzca el ancho del tablero:  ')
         if m.isdigit():
             if int(m) <= 15 and int(m) >= 3:
                 pass
             else:
-                print('Valor fuera del rango establecido')
+                print('Valor fuera del rango establecido\n')
                 menuPrincipal('1')
         else:
-            print('Respuesta Invalida.')
+            print('Respuesta Invalida.\n')
             menuPrincipal('1')
         usuario = input('Por favor, introduzca su nombre de usuario:  ')
         
@@ -97,40 +101,47 @@ def menuPrincipal(resp):
     elif resp == '3':
         return 3
     else:
-        resp = input('Respuesta invalida! Por favor indique una nueva opcion: ')
+        resp = input('Respuesta invalida! Por favor indique una nueva opcion: \n')
         menuPrincipal(resp)
 
 
 def printJuego(tablaUsuario, mainTabla):
-    tablero.print_tablero(tablaUsuario)
-    print('Seleccione una de las siguientes acciones:')
-    print(' [1] Descubrir una baldosa')
-    print(' [2] Guardar la partida')
-    print(' [0] Salir de la partida')
-    opcion = input('Indique su opcion (0, 1 o 2):  ')
-    if opcion.isdigit():
-        mainJuego(tablaUsuario, opcion, mainTabla)
+    global baldosasVacias
+    global baldDesc
+
+    if baldDesc == baldosasVacias:
+        victoria()
     else:
-        print('Respuesta Invalida!')
-        printJuego(tablaUsuario, mainTabla)
+        tablero.print_tablero(tablaUsuario)
+        print('Seleccione una de las siguientes acciones:\n')
+        print(' [1] Descubrir una baldosa\n')
+        print(' [2] Guardar la partida\n')
+        print(' [0] Salir de la partida\n')
+        opcion = input('Indique su opcion (0, 1 o 2):  ')
+        if opcion.isdigit():
+            mainJuego(tablaUsuario, opcion, mainTabla)
+        else:
+            print('Respuesta Invalida!\n')
+            printJuego(tablaUsuario, mainTabla)
 
 
 def mainJuego(tablaUsuario, opcion, mainTabla):
     global baldosasVacias
     global letras
     global baldDesc
+
     if opcion == '1':
         print('Por favor indique las coordenadas de la baldosa que desea descubrir:')
         c1 = input('Coordenada 1 (numeros): ')
         c2 = input('Coordenada 2 (letras): ')
         if not c1.isdigit() or not c2.isalpha():
-            print('Coordenadas invalidas.')
+            print('Coordenadas invalidas.\n')
             mainJuego(tablaUsuario, '1', mainTabla)
         elif int(c1) >= len(tablaUsuario):
-            print('Coordenada fuera del rango valido')
+            print('Coordenada fuera del rango valido\n')
             mainJuego(tablaUsuario, '1', mainTabla)
         elif ord(c2) < ord('A') or ord(c2) > ord(letras[len(tablaUsuario)-1]):
-            print('Coordenada fuera del rango valido')
+            print('Coordenada fuera del rango valido\n')
             mainJuego(tablaUsuario, '1', mainTabla)
 
         else:
@@ -144,6 +155,11 @@ def mainJuego(tablaUsuario, opcion, mainTabla):
 
             if mainTabla[c1][c3] == 'L':
                 gameOver(mainTabla)
+
+            elif tablaUsuario[c1][c3] != ' ':
+                print('Ya has descubierto esta baldosa!\n')
+                printJuego(tablaUsuario, mainTabla)
+
 
             elif c1 == 0:
                 if c3 == 0:
@@ -215,7 +231,54 @@ def mainJuego(tablaUsuario, opcion, mainTabla):
                     tablaUsuario[c1][c3] = minasAledanas
                     baldDesc += 1
                     printJuego(tablaUsuario, mainTabla)
-
+            elif c3 == 0:
+                if mainTabla[c1][c3 + 1] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 - 1][c3 + 1] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 - 1][c3] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 + 1][c3] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 + 1][c3 + 1] == 'L':
+                    minasAledanas += 1
+                tablaUsuario[c1][c3] = minasAledanas
+                baldDesc += 1
+                printJuego(tablaUsuario, mainTabla)
+            elif c3 == len(mainTabla[0])-1:
+                if mainTabla[c1][c3 - 1] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 - 1][c3 - 1] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 - 1][c3] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 + 1][c3] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 + 1][c3 - 1] == 'L':
+                    minasAledanas += 1
+                tablaUsuario[c1][c3] = minasAledanas
+                baldDesc += 1
+                printJuego(tablaUsuario, mainTabla)
+            else:
+                if mainTabla[c1][c3 - 1] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1][c3 + 1] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 - 1][c3 - 1] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 - 1][c3] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 - 1][c3 + 1] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 + 1][c3] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 + 1][c3 - 1] == 'L':
+                    minasAledanas += 1
+                if mainTabla[c1 + 1][c3 + 1] == 'L':
+                    minasAledanas += 1
+                tablaUsuario[c1][c3] = minasAledanas
+                baldDesc += 1
+                printJuego(tablaUsuario, mainTabla)
 
             
                 
@@ -223,6 +286,16 @@ def mainJuego(tablaUsuario, opcion, mainTabla):
         pass
     else:
         pass
+
+
+def victoria():
+    global usuario
+    global puntaje
+    global baldosasVacias
+    global numLegos
+    print('Has descubierto todas las baldosas vacias! Felicidades ' + usuario + ' has ganado!')
+    puntaje = numLegos * baldosasVacias * parametros.POND_PUNT
+    print('Puntaje final: ' + str(puntaje))
 
 
 def gameOver(tabla):                   
@@ -235,6 +308,10 @@ def gameOver(tabla):
     puntaje = numLegos * baldDesc * parametros.POND_PUNT
     print('Puntaje final : ' + str(puntaje))
     rank = open('puntajes.txt', 'a')
+    
+
+def checkPuntajes():
+    pass
 
 
 if __name__ == "__main__":
