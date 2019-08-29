5133cbcdc27f51ec271ee38712a01068216c1d8c
Gaonuk <rodrigogaonagonzalez@gmail.com>
2019-08-29 15:29:17 -0400

    Agregadas las funcionalidades en cargar_datos y consultas

---
+++ Actividades/AC02/cargar_datos.py
@@ -0,0 +1,78 @@
+"""
+Aquí debes completar las funciones propias de Poblar el Sistema
+¡OJO¡: Puedes importar lo que quieras aquí, si no lo necesitas
+"""
+import csv
+from collections import deque
+
+
+
+"""
+Esta estructura de datos te podría ser útil para el desarollo de la actividad, puedes usarla
+si así lo deseas
+"""
+
+DICT_PISOS = {
+    'Chief Tamburini': 'Piso -4',
+    'Jefe': 'Piso -3',
+    'Mentor': 'Piso -2',
+    'Nuevo': 'Piso -1',
+}
+
+
+def cargar_alumnos(ruta_archivo_alumnos):
+    print(f'Cargando datos de {ruta_archivo_alumnos}...')
+    alumnos = list()
+    with open(ruta_archivo_alumnos, 'r', encoding='utf-8') as archivo:
+        for line in archivo:
+            nombre, habilidades = line.split(';')
+            habilidades = habilidades.split(',')
+            habis = set()
+            for habilidad in habilidades:
+                habis.add(habilidad)
+            alumno = (nombre, habis)
+            alumnos.append(alumno)
+
+    return alumnos
+
+
+def cargar_ayudantes(ruta_archivo_ayudantes):
+    print(f'Cargando datos de {ruta_archivo_ayudantes}...')
+    ayudantes = []
+    with open(ruta_archivo_ayudantes, 'r', encoding='utf-8') as archivo:
+        for line in archivo:
+            nombre, rango, debilidades = line.split(';')
+            debilidades = debilidades.split(',')
+            debis = set()
+            for debilidad in debilidades:
+                debis.add(debilidad)
+            comiendo = []
+            ayudante = (nombre, rango, debis, comiendo)
+            ayudantes.append(ayudante)
+    
+    ayudantes_por_piso = {
+        'Piso -1': '',
+        'Piso -2': '',
+        'Piso -3': '',
+        'Piso -4': ''
+    }
+    ayudantes1 = deque()
+    ayudantes2 = deque()
+    ayudantes3 = deque()
+    ayudantes4 = deque()
+    for ayudante in ayudantes:
+        if ayudante[1] == 'Chief Tamburini':
+            ayudantes4.append(ayudante)
+        elif ayudante[1] == 'Jefe':
+            ayudantes3.append(ayudante)
+        elif ayudante[1] == 'Mentor':
+            ayudantes2.append(ayudante)
+        elif ayudante[1] == 'Nuevo':
+            ayudantes1.append(ayudante)
+    
+    ayudantes_por_piso['Piso -1'] = ayudantes1
+    ayudantes_por_piso['Piso -2'] = ayudantes2
+    ayudantes_por_piso['Piso -3'] = ayudantes3
+    ayudantes_por_piso['Piso -4'] = ayudantes4
+
+    return ayudantes_por_piso
---
+++ Actividades/AC02/consultas.py
@@ -0,0 +1,34 @@
+"""
+Aquí debes completar las funciones de las consultas
+"""
+from collections import defaultdict
+
+def resumen_actual(ayudantes, alumnos):
+    print('-'*20)
+    print('Alumnos restantes: {0}'.format(len(alumnos)))
+    ayudantes_totales = 0
+    ayudantes_totales += len(ayudantes['Piso -1'])
+    ayudantes_totales += len(ayudantes['Piso -2'])
+    ayudantes_totales += len(ayudantes['Piso -3'])
+    ayudantes_totales += len(ayudantes['Piso -4'])
+    print('Ayudantes restantes: {0}'.format(ayudantes_totales))
+    print('Ayudantes Piso -1: {0}'.format(len(ayudantes['Piso -1'])))
+    print('Ayudantes Piso -2: {0}'.format(len(ayudantes['Piso -2'])))
+    print('Ayudantes Piso -3: {0}'.format(len(ayudantes['Piso -3'])))
+    print('Ayudantes Piso -4: {0}'.format(len(ayudantes['Piso -4'])))
+    print('-'*20)
+    
+
+
+def stock_comida(alumnos):
+    stock = []
+    comidas = defaultdict(int)
+
+    for alumno in alumnos:
+        for comida in alumnos[1]:
+            comidas[comida] += 1
+
+    for par in comidas.items():
+        stock.append(par)
+
+    return stock
